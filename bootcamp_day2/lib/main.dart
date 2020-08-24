3f9792b13c9192a93f8a73df017f888de1266db8
Imran Sefat <imransefat@gmail.com>
2020-08-24 21:02:03 +0600

    All Buttons are added with floating action button

--- bootcamp_day2/lib/main.dart
+++ bootcamp_day2/lib/main.dart
@@ -23,6 +23,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("Floating button pressed");
+        },
+        child: Text(
+          "=",
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+      ),
       appBar: AppBar(
         centerTitle: true,
         title: Text("Calculator"),
@@ -55,42 +66,245 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Container(
-                height: 80,
-                child: FlatButton(
-                  color: Colors.amber,
-                  onPressed: () {},
-                  child: Text("1"),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "7",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-              Container(
-                height: 80,
-                child: FlatButton(
-                  color: Colors.amber,
-                  onPressed: () {},
-                  child: Text("2"),
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "8",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-              Container(
-                height: 80,
-                child: FlatButton(
-                  color: Colors.amber,
-                  onPressed: () {},
-                  child: Text("3"),
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "9",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-              Container(
-                height: 80,
-                child: FlatButton(
-                  color: Colors.amber,
-                  onPressed: () {},
-                  child: Text("*"),
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "+",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "4",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "5",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "6",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "-",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "1",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "2",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "3",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "*",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      ".",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "0",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "C",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  child: FlatButton(
+                    color: Colors.amber,
+                    onPressed: () {},
+                    child: Text(
+                      "/",
+                      style: TextStyle(
+                        fontSize: 28,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
